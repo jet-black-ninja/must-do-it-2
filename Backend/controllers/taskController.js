cda1472b7b017b24369da4c3e02f97fcf10053d1
jet_black_ninja <pramodkmr958@gmail.com>
2024-09-06 12:07:37 +0530

    error handlers

--- Backend/controllers/taskController.js
+++ Backend/controllers/taskController.js
@@ -1,43 +1,48 @@
 // @desc Get tasks
 //@route GET /api/tasks
 //@access Private
-const getTasks = (req, res) => {
+const asyncHandler = require('express-async-handler');
+const getTasks = asyncHandler(async (req, res) => {
+    if(!req.body.test){
+        res.status(400)
+        throw new Error('please add a text field')
+    }
     res
     .status(200)
     .json({
         message:'get tasks'
     })
-}
+})
 // @desc Create tasks
 //@route POST /api/tasks
 //@access Private
-const postTask = (req,res) =>{
+const postTask = asyncHandler(async (req,res) =>{
     res
     .status(200)
     .json({
         message:`create task`
     })
-}
+})
 // @desc delete tasks
 //@route DELETE /api/tasks/id
 //@access Private
-const deleteTask = (req,res) =>{
+const deleteTask = asyncHandler(async (req,res) =>{
     res
     .status(200)
     .json({
         message: `delete task ${req.params.id}`
     })
-}
+})
 // @desc update tasks
 //@route PUT /api/tasks/id
 //@access Private
-const updateTask =(req,res) => {
+const updateTask = asyncHandler(async (req,res) => {
     res
     .status(200)
     .json({
         message:`update task ${req.params.id}`
     })
-}
+})
 module.exports = {
     getTasks,
     postTask,
