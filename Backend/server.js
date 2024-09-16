7d10b5b940e4a22e44a158b03ba293ac4b9f7cc2
jet_black_ninja <pramodkmr958@gmail.com>
2024-09-16 11:05:55 +0530

    for deployment

--- Backend/server.js
+++ Backend/server.js
@@ -6,6 +6,7 @@ const connectDb = require('./config/db')
 const port = process.env.PORT || 5000;
 const cors= require('cors');
 const app = express();
+const path = require('path');
 connectDb();
 
 app.use(express.json());
@@ -19,7 +20,11 @@ app.use(cors({
 //
 app.use('/api/users', require("./routes/userRoutes"));
 app.use('/api/tasks', require("./routes/taskRoutes"));
-
+//serve frontend 
+if(process.env.NODE_ENV ==='production'){
+    app.use(express.static(path.join(__dirname, '../frontend/dist')));
+    app.get('*',(req,res)=> res.sendFile(path.resolve(__dirname,'../','frontend','dist','index.html')) )
+}
 app.use(errorHandler)
 app.listen(port , () => console.log(`server started on ${port}`));
 
