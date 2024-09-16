35345ef9665c3129430335b54ed2393d3884982d
jet_black_ninja <pramodkmr958@gmail.com>
2024-09-16 11:13:10 +0530

    vercel

--- Backend/server.js
+++ Backend/server.js
@@ -24,6 +24,10 @@ app.use('/api/tasks', require("./routes/taskRoutes"));
 if(process.env.NODE_ENV ==='production'){
     app.use(express.static(path.join(__dirname, '../frontend/dist')));
     app.get('*',(req,res)=> res.sendFile(path.resolve(__dirname,'../','frontend','dist','index.html')) )
+}else{
+    app.get('/',(req,res)=>{
+        res.send('Please set to production');
+    })
 }
 app.use(errorHandler)
 app.listen(port , () => console.log(`server started on ${port}`));
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import Header from './components/header';
+import Header from './components/Header';
 import Dashboard from './pages/Dashboard';
 import Login from './pages/Login';
 import Register from './pages/Register';
